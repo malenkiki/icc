6807579af041b13e17fc9727b803e2e9d269c45e
Michel Petit <petit.michel@gmail.com>
2014-01-23 14:00:32 +0100

    New extracted fields

--- src/Malenki/Icc.php
+++ src/Malenki/Icc.php
@@ -33,7 +33,34 @@ namespace Malenki;
  */
 class Icc
 {
+    protected static $arr_primary_platforms = array(
+        'APPL' => 'Apple Computer, Inc.',
+        'MSFT' => 'Microsoft Corporation',
+        'SGI'  => 'Silicon Graphics, Inc.',
+        'SUNW' => 'Sun Microsystems, Inc.'
+    );
+
+    protected static $arr_profile_classes = array(
+        'scnr' => 'Input device profile',
+        'mntr' => 'Display device profile',
+        'prtr' => 'Output device profile',
+        'link' => 'DeviceLink profile',
+        'spac' => 'ColorSpace profile',
+        'abst' => 'Abstract profile',
+        'nmcl' => 'NamedColor profile'
+    );
+
     protected $fp = null;
+
+    protected $header = null;
+
+    public function __get($name)
+    {
+        if($name == 'header')
+        {
+            return $this->$name;
+        }
+    }
 
 
 
@@ -41,13 +68,19 @@ class Icc
     {
         $this->fp = fopen($str_file, 'rb');
 
-        $this->extractProfileSize();
-        $this->extractPreferedCmmType();
-        $this->extractProfileVersion();
-        $this->extractProfileDeviceClass();
-        $this->extractColourSpaceOfData();
-        $this->extractPcs();
-        $this->extractDateTime();
+        $this->header = new \stdClass();
+
+        $this->header->profileSize = $this->extractProfileSize();
+        $this->header->preferedCmmType = $this->extractPreferedCmmType();
+        $this->header->profileVersion = $this->extractProfileVersion();
+        $this->header->profileDeviceClass = $this->extractProfileDeviceClass();
+        $this->header->colourSpace = $this->extractColourSpaceOfData();
+        $this->header->pcs = $this->extractPcs();
+        $this->header->DateTime = $this->extractDateTime();
+        $this->header->profileFileSignature = $this->extractProfileFileSignature(); // should be 'acsp', if not error -> todo: do test for that
+        $this->header->primaryPlatformSignature = $this->extractPrimaryPlatformSignature();
+        $this->header->profileFlags = $this->extractProfileFlags();
+        $this->header->manufacturer = $this->extractManufacturer();
     }
 
 
@@ -59,7 +92,7 @@ class Icc
 
     private function extractProfileSize()
     {
-        $arr = unpack("C4", fread($fp, 4));
+        $arr = unpack("C4", fread($this->fp, 4));
         // Workaround
         foreach($arr as $k => $b)
         {
@@ -71,7 +104,7 @@ class Icc
 
     private function extractPreferedCmmType()
     {
-        $arr = unpack("A4", fread($fp, 4));
+        $arr = unpack("A4", fread($this->fp, 4));
         return array_pop($arr);
     }
 
@@ -80,7 +113,7 @@ class Icc
     {
         $arr_out = array();
 
-        $arr = unpack("C4", fread($fp, 4));
+        $arr = unpack("C4", fread($this->fp, 4));
         $bin = str_pad(decbin($arr[2]), 8, '0', STR_PAD_LEFT);
         $arr_out[] = $arr[1];
         $arr_out[] = bindec(substr($bin, 0, 4)); //minor version
@@ -92,29 +125,95 @@ class Icc
 
     private function extractProfileDeviceClass()
     {
-        $arr = unpack("A4", fread($fp, 4));
-        return array_pop($arr);
+        $out = new \stdClass();
+        $arr = unpack("A4", fread($this->fp, 4));
+        $out->signature = array_pop($arr);
+        $out->name = self::$arr_profile_classes[$out->signature];
+
+        return $out;
     }
 
 
+    // TODO add name too
     private function extractColourSpaceOfData()
     {
-        $arr = unpack("A4", fread($fp, 4));
+        $arr = unpack("A4", fread($this->fp, 4));
         return array_pop($arr);
     }
 
 
     private function extractPcs()
     {
-        $arr = unpack("A4", fread($fp, 4));
+        $arr = unpack("A4", fread($this->fp, 4));
         return array_pop($arr);
     }
 
 
-    // TODO
     private function extractDateTime()
     {
-        $arr = unpack("A12", fread($fp, 12));
-        var_dump(array_pop($arr));
+        $arr = unpack("C12", fread($this->fp, 12));
+
+        $date = new \stdClass();
+        $date->year = hexdec(dechex($arr[1]).dechex($arr[2]));
+        $date->month = hexdec(dechex($arr[3]).dechex($arr[4]));
+        $date->day = hexdec(dechex($arr[5]).dechex($arr[6]));
+        $date->hours = hexdec(dechex($arr[7]).dechex($arr[8]));
+        $date->minutes = hexdec(dechex($arr[9]).dechex($arr[10]));
+        $date->seconds = hexdec(dechex($arr[11]).dechex($arr[12]));
+        $date->str = sprintf(
+            '%04d-%02d-%02d %02d:%02d:%02d',
+            $date->year,
+            $date->month,
+            $date->day,
+            $date->hours,
+            $date->minutes,
+            $date->seconds
+        );
+
+        return $date;
+    }
+
+
+
+    private function extractProfileFileSignature()
+    {
+        $arr = unpack("A4", fread($this->fp, 4));
+        return array_pop($arr);
+    }
+
+
+
+    private function extractPrimaryPlatformSignature()
+    {
+        $out = new \stdClass();
+        $arr = unpack("A4", fread($this->fp, 4));
+        $out->signature = array_pop($arr);
+
+        if(array_key_exists($out->signature, self::$arr_primary_platforms))
+        {
+            $out->exists = true;
+            $out->name = self::$arr_primary_platforms[$out->signature];
+        }
+        else
+        {
+            $out->exists = false;
+            $out->name = null;
+        }
+
+        return $out;
+    }
+    
+    
+    // TODO
+    private function extractProfileFlags()
+    {
+        $arr = unpack("C4", fread($this->fp, 4));
+        return $arr;
+    }
+    
+    private function extractManufacturer()
+    {
+        $arr = unpack("C4", fread($this->fp, 4));
+        return $arr;
     }
 }
